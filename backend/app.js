83e29fe77805f39bf708d7d373a92158f80ea257
akmalariq9 <akmalariq009@gmail.com>
2023-05-10 21:21:32 +0700

    feat: update cors url

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'https://nusantara-store-z34b.vercel.app/',
+  origin: 'https://nusantara-store-z34b.vercel.app',
   credentials: true
 }));
 
