46830f2f78923ecfa47dfc1271447a68cdde5a80
akmalariq9 <akmalariq009@gmail.com>
2023-05-11 09:43:30 +0700

    feat: add parameter backend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://nusantara-store.vercel.app/api/v2";
+export const server = "https://nusantara-store.vercel.app/api/v2/";
 
 export const backend_url = "https://nusantara-store.vercel.app/";
